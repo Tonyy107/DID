d5be0f82cdf55493976ae0fa6b151d3c78c905f3
Tonyy107 <antonmagdy53@gmail.com>
2025-04-30 01:04:58 +0300

    gameplay_on_fire

--- scenes/character_body_2d.gd
+++ scenes/character_body_2d.gd
@@ -1,28 +1,28 @@
 extends CharacterBody2D
 
-# Movement
+# Movement settings
 const SPEED = 200
 const CROUCH_SPEED = 100
 const ACCELERATION = 1200.0
 const FRICTION = 800.0
 
-# Jumping
+# Jumping settings
 const JUMP_FORCE = -400.0
 const DOUBLE_JUMP_FORCE = -350.0
-const GRAVITY = 1000.0
+const GRAVITY = 1000
 const MAX_FALL_SPEED = 900.0
 const COYOTE_TIME = 0.15
 const JUMP_BUFFER_TIME = 0.1
 
-# Dashing
+# Dashing settings
 const DASH_SPEED = 600.0
 const DASH_DURATION = 0.15
 const DASH_COOLDOWN = 0.5
 
-# Wall jumping
+# Wall jump settings
 const WALL_JUMP_FORCE = Vector2(300, -400)
 
-# States
+# State variables
 var can_double_jump := true
 var has_double_jumped := false
 var coyote_timer := 0.0
@@ -31,12 +31,9 @@ var is_crouching := false
 var is_dashing := false
 var dash_timer := 0.0
 var dash_cooldown_timer := 0.0
-
-# Input
-var input_direction := 0
+var input_direction := 0.0
 
 # Nodes
-@onready var animated_sprite = $animated_sprite
 @onready var wall_check_left = $wall_check_left
 @onready var wall_check_right = $wall_check_right
 
@@ -44,18 +41,21 @@ func _physics_process(delta):
 	handle_input()
 	handle_dash(delta)
 	apply_gravity(delta)
-	move_character(delta)
 	jump_logic()
-	handle_animation()
+	move_character(delta)
+	move_and_slide()  # In Godot 4, no arguments are needed
 
 func handle_input():
+	# Get horizontal input direction
 	input_direction = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	is_crouching = Input.is_action_pressed("ui_down")
 
+	# Jump buffering
 	if Input.is_action_just_pressed("ui_accept"):
 		jump_buffer_timer = JUMP_BUFFER_TIME
 
-	if Input.is_action_just_pressed("dash") and dash_cooldown_timer <= 0 and not is_dashing:
+	# Start dash if ready
+	if Input.is_action_just_pressed("dash") and dash_cooldown_timer <= 0.0 and not is_dashing:
 		is_dashing = true
 		dash_timer = DASH_DURATION
 		dash_cooldown_timer = DASH_COOLDOWN
@@ -69,7 +69,8 @@ func apply_gravity(delta):
 		velocity.y = 0
 		has_double_jumped = false
 		coyote_timer = COYOTE_TIME
-	
+
+	# Update timers
 	if coyote_timer > 0:
 		coyote_timer -= delta
 	if jump_buffer_timer > 0:
@@ -78,11 +79,11 @@ func apply_gravity(delta):
 func move_character(delta):
 	if is_dashing:
 		velocity.y = 0
-		velocity.x = DASH_SPEED * sign(input_direction if input_direction != 0 else 1)
-		move_and_slide()
+		var dash_direction = input_direction if input_direction != 0 else 1
+		velocity.x = DASH_SPEED * sign(dash_direction)
 		return
 
-	var current_speed = is_crouching ? CROUCH_SPEED : SPEED
+	var current_speed = CROUCH_SPEED if is_crouching else SPEED
 	var target_speed = input_direction * current_speed
 
 	if abs(target_speed) > 0.1:
@@ -90,19 +91,17 @@ func move_character(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
 
-	move_and_slide()
-
 func handle_dash(delta):
 	if is_dashing:
 		dash_timer -= delta
-		if dash_timer <= 0:
+		if dash_timer <= 0.0:
 			is_dashing = false
-	if dash_cooldown_timer > 0:
+	if dash_cooldown_timer > 0.0:
 		dash_cooldown_timer -= delta
 
 func jump_logic():
 	if jump_buffer_timer > 0:
-		# Normal jump
+		# Normal jump or coyote time jump
 		if is_on_floor() or coyote_timer > 0:
 			velocity.y = JUMP_FORCE
 			jump_buffer_timer = 0
@@ -123,23 +122,6 @@ func jump_logic():
 			has_double_jumped = true
 			jump_buffer_timer = 0
 
+	# Variable jump height (if jump released early)
 	if Input.is_action_just_released("ui_accept") and velocity.y < 0:
 		velocity.y *= 0.5
-
-func handle_animation():
-	if is_dashing:
-		animated_sprite.play("dash")
-	elif not is_on_floor():
-		if velocity.y < 0:
-			animated_sprite.play("jump")
-		else:
-			animated_sprite.play("fall")
-	elif is_crouching:
-		animated_sprite.play("crouch")
-	elif abs(velocity.x) > 10:
-		animated_sprite.play("run")
-	else:
-		animated_sprite.play("idle")
-
-	if velocity.x != 0:
-		animated_sprite.flip_h = velocity.x < 0
