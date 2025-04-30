9331c97ea010a3202b0f89afc37f92b8ddb21884
Tonyy107 <antonmagdy53@gmail.com>
2025-04-30 16:55:34 +0300

    crouch and wall slide

--- scenes/character_body_2d.gd
+++ scenes/character_body_2d.gd
@@ -1,28 +1,29 @@
 extends CharacterBody2D
 
-# Movement settings
-const SPEED = 200
-const CROUCH_SPEED = 100
+# Movement constants
+const SPEED = 200.0
+const CROUCH_SPEED = 100.0
 const ACCELERATION = 1200.0
 const FRICTION = 800.0
 
-# Jumping settings
+# Jumping constants
 const JUMP_FORCE = -400.0
 const DOUBLE_JUMP_FORCE = -350.0
-const GRAVITY = 1000
+const GRAVITY = 1000.0
 const MAX_FALL_SPEED = 900.0
 const COYOTE_TIME = 0.15
 const JUMP_BUFFER_TIME = 0.1
+const WALL_SLIDE_SPEED = 100.0
 
-# Dashing settings
+# Dashing constants
 const DASH_SPEED = 600.0
 const DASH_DURATION = 0.15
 const DASH_COOLDOWN = 0.5
 
-# Wall jump settings
+# Wall jumping constants
 const WALL_JUMP_FORCE = Vector2(300, -400)
 
-# State variables
+# States
 var can_double_jump := true
 var has_double_jumped := false
 var coyote_timer := 0.0
@@ -31,30 +32,47 @@ var is_crouching := false
 var is_dashing := false
 var dash_timer := 0.0
 var dash_cooldown_timer := 0.0
+
+# Input variables
 var input_direction := 0.0
+var last_facing_direction := 1.0
+
+# Load separate shape resources
+@onready var stand_shape = preload("res://shapes/stand_shape.tres")
+@onready var crouch_shape = preload("res://shapes/crouch_shape.tres")
 
 # Nodes
 @onready var wall_check_left = $wall_check_left
 @onready var wall_check_right = $wall_check_right
+@onready var collider = $CollisionShape2D
+@onready var ceiling_check = $ceiling_check
 
 func _physics_process(delta):
 	handle_input()
 	handle_dash(delta)
 	apply_gravity(delta)
-	jump_logic()
 	move_character(delta)
-	move_and_slide()  # In Godot 4, no arguments are needed
+	jump_logic(delta)
+	move_and_slide()
 
 func handle_input():
-	# Get horizontal input direction
-	input_direction = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	is_crouching = Input.is_action_pressed("ui_down")
+	input_direction = Input.get_axis("ui_left", "ui_right")
+	if input_direction != 0:
+		last_facing_direction = input_direction
 
-	# Jump buffering
+	# Crouch logic
+	if Input.is_action_pressed("ui_down"):
+		is_crouching = true
+	elif not ceiling_check.is_colliding():
+		is_crouching = false
+
+	collider.shape = crouch_shape if is_crouching else stand_shape
+
+	# Jump buffer
 	if Input.is_action_just_pressed("ui_accept"):
 		jump_buffer_timer = JUMP_BUFFER_TIME
 
-	# Start dash if ready
+	# Dash input
 	if Input.is_action_just_pressed("dash") and dash_cooldown_timer <= 0.0 and not is_dashing:
 		is_dashing = true
 		dash_timer = DASH_DURATION
@@ -62,25 +80,27 @@ func handle_input():
 
 func apply_gravity(delta):
 	if not is_on_floor():
-		if not is_dashing:
+		var wall_normal = get_wall_normal()
+		if is_on_wall_only() and velocity.y > 0.0 and wall_normal != Vector2.ZERO:
+			velocity.y = min(velocity.y + GRAVITY * delta, WALL_SLIDE_SPEED)
+		elif not is_dashing:
 			velocity.y += GRAVITY * delta
 			velocity.y = min(velocity.y, MAX_FALL_SPEED)
 	else:
-		velocity.y = 0
+		velocity.y = 0.0
 		has_double_jumped = false
+		can_double_jump = true
 		coyote_timer = COYOTE_TIME
 
 	# Update timers
-	if coyote_timer > 0:
-		coyote_timer -= delta
-	if jump_buffer_timer > 0:
-		jump_buffer_timer -= delta
+	coyote_timer = max(coyote_timer - delta, 0.0)
+	jump_buffer_timer = max(jump_buffer_timer - delta, 0.0)
 
 func move_character(delta):
 	if is_dashing:
-		velocity.y = 0
-		var dash_direction = input_direction if input_direction != 0 else 1
-		velocity.x = DASH_SPEED * sign(dash_direction)
+		velocity.y = 0.0
+		var dash_dir = input_direction if input_direction != 0 else last_facing_direction
+		velocity.x = DASH_SPEED * dash_dir
 		return
 
 	var current_speed = CROUCH_SPEED if is_crouching else SPEED
@@ -89,39 +109,32 @@ func move_character(delta):
 	if abs(target_speed) > 0.1:
 		velocity.x = move_toward(velocity.x, target_speed, ACCELERATION * delta)
 	else:
-		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
+		velocity.x = move_toward(velocity.x, 0.0, FRICTION * delta)
 
 func handle_dash(delta):
 	if is_dashing:
 		dash_timer -= delta
 		if dash_timer <= 0.0:
 			is_dashing = false
-	if dash_cooldown_timer > 0.0:
-		dash_cooldown_timer -= delta
+			velocity.x = 0.0
+	dash_cooldown_timer = max(dash_cooldown_timer - delta, 0.0)
 
-func jump_logic():
-	if jump_buffer_timer > 0:
-		# Normal jump or coyote time jump
-		if is_on_floor() or coyote_timer > 0:
+func jump_logic(_delta):
+	if jump_buffer_timer > 0.0:
+		if is_on_floor() or coyote_timer > 0.0:
 			velocity.y = JUMP_FORCE
-			jump_buffer_timer = 0
-			coyote_timer = 0
-		# Wall jump
+			jump_buffer_timer = 0.0
+			coyote_timer = 0.0
 		elif is_on_wall():
-			var wall_dir = 0
-			if wall_check_left.is_colliding():
-				wall_dir = 1
-			elif wall_check_right.is_colliding():
-				wall_dir = -1
-			if wall_dir != 0:
-				velocity = WALL_JUMP_FORCE * Vector2(wall_dir, 1)
-				jump_buffer_timer = 0
-		# Double jump
-		elif not has_double_jumped and can_double_jump:
+			var wall_normal = get_wall_normal()
+			if wall_normal != Vector2.ZERO:
+				velocity = Vector2(WALL_JUMP_FORCE.x * -wall_normal.x, WALL_JUMP_FORCE.y)
+				jump_buffer_timer = 0.0
+		elif can_double_jump:
 			velocity.y = DOUBLE_JUMP_FORCE
-			has_double_jumped = true
-			jump_buffer_timer = 0
+			can_double_jump = false
+			jump_buffer_timer = 0.0
 
-	# Variable jump height (if jump released early)
-	if Input.is_action_just_released("ui_accept") and velocity.y < 0:
+	# Jump cut
+	if Input.is_action_just_released("ui_select") and velocity.y < 0.0:
 		velocity.y *= 0.5
